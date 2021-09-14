146df538c9bb504968443d74077b9947cee4a807
technicakidz <y.technicakidz08@gmail.com>
2021-09-14 23:15:37 +0900

    add: backend dir

---
+++ backend/core.py
@@ -0,0 +1,20 @@
+
+input(w_bill, e_bill, g_fee, i_fee)
+
+rent = 60000
+util_cost = sum(w_bill, e_bill, g_fee, i_fee)
+live_cost = sum(x, y)
+
+def pico_pay(util_cost, rent):
+    diff = live_cost / 2
+    if util_cost.isdecimal():
+        hoge = util_cost/2 + rent
+        total = hoge + diff
+    print(total)
+
+# 光熱費(int) = 水 + 電気 + ガス + ネット
+# A = 光熱費 / 2 + 60,000(家賃)
+# 生活費(int) = x + y
+# B = 一人当たりの生活費 / 2
+# B は差分(unsigned char)
+# ans(int) = A + (+-)B
---
+++ backend/server.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask import request, make_response, jsonify
+from flask_cors import CORS
+# from core import logic
+
+app = Flask(__name__, static_folder="./build/static", template_folder="./build")
+CORS(app) #Cross Origin Resource Sharing
+
+@app.route("/", methods=['GET'])
+def index():
+    return "text parser:)"
+
+@app.route("/logic", methods=['GET','POST'])
+def parse():
+    #print(request.get_json())
+    data = request.get_json()
+    text = data['post_text']
+
+    res = logic(text)
+    response = {'result': res}
+    #print(response)
+    return make_response(jsonify(response))
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host='127.0.0.1', port=5000)
