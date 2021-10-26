61921576544f10a42012bf3b38abf7901469f81c
technicakidz <y.technicakidz08@gmail.com>
2021-10-27 01:14:31 +0900

    script 単独で動くように

--- backend/pay.py
+++ backend/pay.py
@@ -1,11 +1,13 @@
+# FIX: 変数で値受け取り
 # input(bill_w, bill_e, fee_gas, fee_i)
-
+[bill_w, bill_e, fee_gas, fee_i] = [1, 2, 3, 4]
+a:int = 15000
+b:int = 10000
 base = 60000
 # 光熱費:int = 水 + 電気 + ガス + ネット
-util_cost = sum(bill_w, bill_e, fee_gas, fee_i)
+util_cost = bill_w + bill_e + fee_gas + fee_i
 # 生活費:int = a + b
-live_cost = sum(a, b)
-
+live_cost = a + b
 
 # A:float = 光熱費 / 2 + 60,000[base]
 # B:float = 生活費(total_a + total_b) / 2
@@ -21,3 +23,7 @@ def pay(util_cost, live_cost, a, b, base):
         print(total)
     else:
         print("error")
+
+# DEBUG
+print(util_cost)
+pay(util_cost, live_cost, a, b, base)
